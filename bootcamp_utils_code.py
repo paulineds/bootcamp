f426af5054f49379ce81b91d1b71ddb81495c504
paulinedurand <pdurand@caltech.edu>
2017-06-22 12:58:12 -0700

    updated bootcamp_utils_code with bootstrap functions in it

--- bootcamp_utils_code.py
+++ bootcamp_utils_code.py
@@ -1,6 +1,7 @@
 #in this file you can find usefull code such as the ECDF function
 #to caal one of the function in this file use 'import bootcamp_utils_code as 'cdf'or other alias
 #then call the function in the code with  alias.ecdf(data) (alias.functionin fileyouwanttouse(data))
+#bootstrap function for stat analysis
 
 
 import numpy as np
@@ -12,3 +13,15 @@ def ecdf(data):
     x = np.sort(data)
     y = np.arange(1,len(data)+1)/float(len(data))
     return (x,y)
+
+
+#bootstrap of the function chosen in argument (mean but can be standard dev...)
+def bs_replicate(data, func=np.mean):
+    """Compute a bootstrap replicate from data."""
+    bs_sample = np.random.choice(data, size=len(data))
+
+    return func(bs_sample)
+
+def draw_bs_reps(data, func=np.mean, size=10000):
+    """draw bootstrap replicate from 1d data"""
+    return np.array([bs_replicate(data, func=func) for _ in range(size)])
