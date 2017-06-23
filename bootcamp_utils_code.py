1ba7b607745e1dcdb857f7b3caf785841ec8bd45
paulinedurand <pdurand@caltech.edu>
2017-06-23 09:06:18 -0700

    updated bootcamp_utils_code with ecdf for data frame and formal ecdf

--- bootcamp_utils_code.py
+++ bootcamp_utils_code.py
@@ -3,16 +3,22 @@
 #then call the function in the code with  alias.ecdf(data) (alias.functionin fileyouwanttouse(data))
 #bootstrap function for stat analysis
 
+# Plotting modules and settings.
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+# Pandas, conventionally imported as pd
+import pandas as pd
 
 import numpy as np
 
 
-def ecdf(data):
-    """ Compute the empiric cumulative distribution of the data"""
+#def ecdf(data):
+#    """ Compute the empiric cumulative distribution of the data"""
 
-    x = np.sort(data)
-    y = np.arange(1,len(data)+1)/float(len(data))
-    return (x,y)
+#    x = np.sort(data)
+#    y = np.arange(1,len(data)+1)/float(len(data))
+#    return (x,y)
 
 
 #bootstrap of the function chosen in argument (mean but can be standard dev...)
@@ -25,3 +31,177 @@ def bs_replicate(data, func=np.mean):
 def draw_bs_reps(data, func=np.mean, size=10000):
     """draw bootstrap replicate from 1d data"""
     return np.array([bs_replicate(data, func=func) for _ in range(size)])
+
+def ecdf(data, formal=False, buff=0.1, min_x=None, max_x=None):
+    """
+    Generate `x` and `y` values for plotting an ECDF.
+
+    Parameters
+    ----------
+    data : array_like
+        Array of data to be plotted as an ECDF.
+    formal : bool, default False
+        If True, generate `x` and `y` values for formal ECDF.
+        Otherwise, generate `x` and `y` values for "dot" style ECDF.
+    buff : float, default 0.1
+        How long the tails at y = 0 and y = 1 should extend as a
+        fraction of the total range of the data. Ignored if
+        `formal` is False.
+    min_x : float, default None
+        Minimum value of `x` to include on plot. Overrides `buff`.
+        Ignored if `formal` is False.
+    max_x : float, default None
+        Maximum value of `x` to include on plot. Overrides `buff`.
+        Ignored if `formal` is False.
+
+    Returns
+    -------
+    x : array
+        `x` values for plotting
+    y : array
+        `y` values for plotting
+    """
+
+    if formal:
+        return _ecdf_formal(data, buff=buff, min_x=min_x, max_x=max_x)
+    else:
+        return _ecdf_dots(data)
+
+def _ecdf_dots(data):
+    """
+    Compute `x` and `y` values for plotting an ECDF.
+
+    Parameters
+    ----------
+    data : array_like
+        Array of data to be plotted as an ECDF.
+
+    Returns
+    -------
+    x : array
+        `x` values for plotting
+    y : array
+        `y` values for plotting
+    """
+    return np.sort(data), np.arange(1, len(data)+1) / float(len(data))
+
+def _ecdf_formal(data, buff=0.1, min_x=None, max_x=None):
+    """
+    Generate `x` and `y` values for plotting a formal ECDF.
+
+    Parameters
+    ----------
+    data : array_like
+        Array of data to be plotted as an ECDF.
+    buff : float, default 0.1
+        How long the tails at y = 0 and y = 1 should extend as a fraction
+        of the total range of the data.
+    min_x : float, default None
+        Minimum value of `x` to include on plot. Overrides `buff`.
+    max_x : float, default None
+        Maximum value of `x` to include on plot. Overrides `buff`.
+
+    Returns
+    -------
+    x : array
+        `x` values for plotting
+    y : array
+        `y` values for plotting
+    """
+    # Get x and y values for data points
+    x, y = _ecdf_dots(data)
+
+    # Set defaults for min and max tails
+    if min_x is None:
+        min_x = x[0] - (x[-1] - x[0])*buff
+    if max_x is None:
+        max_x = x[-1] + (x[-1] - x[0])*buff
+
+    # Set up output arrays
+    x_formal = np.empty(2*(len(x) + 1))
+    y_formal = np.empty(2*(len(x) + 1))
+
+    # y-values for steps
+    y_formal[:2] = 0
+    y_formal[2::2] = y
+    y_formal[3::2] = y
+
+    # x- values for steps
+    x_formal[0] = min_x
+    x_formal[1] = x[0]
+    x_formal[2::2] = x
+    x_formal[3:-1:2] = x[1:]
+    x_formal[-1] = max_x
+
+    return x_formal, y_formal
+
+
+def ecdf_plot(data, value, hue=None, formal=False, buff=0.1, min_x=None, max_x=None,
+              ax=None):
+    """
+    Generate `x` and `y` values for plotting an ECDF.
+
+    Parameters
+    ----------
+    data : Pandas DataFrame
+        Tidy DataFrame with data sets to be plotted.
+    value : column name of DataFrame
+        Name of column that contains data to make ECDF with.
+    hue : column name of DataFrame
+        Name of column that identifies labels of data. A seperate
+        ECDF is plotted for each unique entry.
+    formal : bool, default False
+        If True, generate `x` and `y` values for formal ECDF.
+        Otherwise, generate `x` and `y` values for "dot" style ECDF.
+    buff : float, default 0.1
+        How long the tails at y = 0 and y = 1 should extend as a
+        fraction of the total range of the data. Ignored if
+        `formal` is False.
+    min_x : float, default None
+        Minimum value of `x` to include on plot. Overrides `buff`.
+        Ignored if `formal` is False.
+    max_x : float, default None
+        Maximum value of `x` to include on plot. Overrides `buff`.
+        Ignored if `formal` is False.
+    ax : matplotlib Axes
+        Axes object to draw the plot onto, otherwise makes a new
+        figure/axes.
+
+    Returns
+    -------
+    output : matplotlib Axes
+        Axes object containg ECDFs.
+    """
+
+    # Set up axes
+    if ax is None:
+        fig, ax = plt.subplots(1, 1)
+        ax.set_xlabel(str(value))
+        ax.set_ylabel('ECDF')
+
+    if hue is None:
+        x, y = ecdf(data[value], formal=formal, buff=buff, min_x=min_x, max_x=max_x)
+
+        # Make plots
+        if formal:
+            _ = ax.plot(x, y)
+        else:
+            _ = ax.plot(x, y, marker='.', linestyle='none')
+    else:
+        gb = data.groupby(hue)
+        ecdfs = gb[value].apply(ecdf, formal=formal, buff=buff, min_x=min_x, max_x=max_x)
+
+        # Make plots
+        if formal:
+            for i, xy in ecdfs.iteritems():
+                _ = ax.plot(*xy)
+        else:
+            for i, xy in ecdfs.iteritems():
+                _ = ax.plot(*xy, marker='.', linestyle='none')
+
+        # Add legend
+        ax.legend(ecdfs.index, loc=0)
+
+    return ax
+
+ax = ecdf_plot(df, 'impact force (mN)', hue='ID')
